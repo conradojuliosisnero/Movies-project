ae94a9423896f84ecaffa569448968967f26a30f
conrado julio <conradojuliosisnero@gmail.com>
2023-07-23 12:05:03 -0500

    conexion exitosa a la API de peliculas

--- Js/Conection-API.js
+++ Js/Conection-API.js
@@ -1,15 +1,36 @@
 // variables
 
-const UrlMovies = "https://api.themoviedb.org/3/movie/changes?api_key=6684d118538ed2ff3c2882fe2b6e2e2e&language=es-MX";
+const urlMovies = "https://api.themoviedb.org/3/movie/popular?api_key=6684d118538ed2ff3c2882fe2b6e2e2e&language=es-MX";
+const urlPosters = "https://image.tmdb.org/t/p/w500/";
 
 // comenzamos creando una funcion para conectarnos a la api
 const cargarPeliculas = async () => {
 	try {
-		const respuesta = await fetch(UrlMovies); // esperamos la respuesta de la api
-		console.log(respuesta); //comprobamos la respuesta
-		const data = await respuesta.json(); // esperamos acceder al json
-		console.log(data); // comprobamos el json
-	} catch (error) { // manejamos el error
+		const respuesta = await fetch(urlMovies);   // esperamos la respuesta de la API    
+        const data = await respuesta.json();    // obtenemos los datos del json
+        const resultados = data.results;    // accedemos a los resultados de peliculas
+        // si la respuesta es valida 
+        if (respuesta.status == 200) {
+                let peliculas = '';  
+                resultados.forEach( pelicula => {       // iteramos sobre cada titulo del arreglo
+                    peliculas += `
+                        <div class="pelicula">
+                            <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
+                            <h3 class="titulo">${pelicula.title}</h3>
+                        </div>
+                    `;
+                });
+
+                document.getElementById('contenedor').innerHTML = peliculas;
+
+                
+            } else if(respuesta.status == 404){
+                console.log("no se encontro el sitio")
+            }else if(respuesta.status == 401){
+                console.log("no se encontro el sitio deseado")
+            }
+        
+	} catch (error) {      // manejamos el error
 		console.log(error);
 	}
 };
