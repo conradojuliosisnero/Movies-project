7d40fc41e122ce88ff1e8fd49908b08e477e08e5
conrado julio <conradojuliosisnero@gmail.com>
2023-07-23 12:45:12 -0500

    botones agregados con su funcionalidad

--- Js/Conection-API.js
+++ Js/Conection-API.js
@@ -1,18 +1,35 @@
 // variables
+let pagina = 1;
+// colocamos a funcionar los botones 
+const btnDerecho = document.getElementById('btnSiguiente');
+const btnIzquierdo = document.getElementById('btnAnterior');
 
-const urlMovies = "https://api.themoviedb.org/3/movie/popular?api_key=6684d118538ed2ff3c2882fe2b6e2e2e&language=es-MX";
-const urlPosters = "https://image.tmdb.org/t/p/w500/";
+btnDerecho.addEventListener('click',() => {
+    if (pagina < 1000) {
+        pagina += 1;
+        cargarPeliculas();
+    }
+});
+
+btnIzquierdo.addEventListener('click',() => {
+    if (pagina > 1) {
+        pagina -= 1;
+        cargarPeliculas();
+    }
+});
 
 // comenzamos creando una funcion para conectarnos a la api
 const cargarPeliculas = async () => {
 	try {
-		const respuesta = await fetch(urlMovies);   // esperamos la respuesta de la API    
+		const respuesta = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=6684d118538ed2ff3c2882fe2b6e2e2e&language=es-MX&page=${pagina}`);   // esperamos la respuesta de la API    
         const data = await respuesta.json();    // obtenemos los datos del json
         const resultados = data.results;    // accedemos a los resultados de peliculas
         // si la respuesta es valida 
         if (respuesta.status == 200) {
                 let peliculas = '';  
                 resultados.forEach( pelicula => {       // iteramos sobre cada titulo del arreglo
+                    
+                    // accedemos a  las imagenes de las peliculas
                     peliculas += `
                         <div class="pelicula">
                             <img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
@@ -20,10 +37,9 @@ const cargarPeliculas = async () => {
                         </div>
                     `;
                 });
-
+                // seleccionamos el contenedor
                 document.getElementById('contenedor').innerHTML = peliculas;
 
-                
             } else if(respuesta.status == 404){
                 console.log("no se encontro el sitio")
             }else if(respuesta.status == 401){
