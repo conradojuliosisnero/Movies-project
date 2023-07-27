505f17b9a9023793983cb0dbc48d74adc9cf5c95
conrado julio <conradojuliosisnero@gmail.com>
2023-07-27 10:59:52 -0500

    scroll infinito terminado

--- Js/Conection-API.js
+++ Js/Conection-API.js
@@ -1,64 +1,81 @@
-const btnAnterior = document.getElementById("btnAnterior");
-const btnSiguiente = document.getElementById("btnSiguiente");
 let pagina = 1;
-const urlApi = `https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`;
-
-// btnSiguiente.addEventListener('click', () => {
-// 	if(pagina < 1000){
-// 		pagina += 1;
-// 		cargarPeliculas();
-// 	}
-// });
-
-// btnAnterior.addEventListener('click', () => {
-// 	if(pagina > 1){
-// 		pagina -= 1;
-// 		cargarPeliculas();
-// 	}
-// });
-
-//	 opciones del observador
+let peliculas = '';
+let ultimaPelicula;
 
 const optionsObserver = {
 	root: null,
-	rootMargin: "0px",
+	rootMargin: "0px 0px 350px 0px",
 	threshold: 1.0,
 };
 
-const observer = new IntersectionObserver((entrada, observador) => {}, optionsObserver);
+const observador = new IntersectionObserver((entradas,observador) => {
 
-const moviesViewport = document.querySelectorAll('.contenedor .pelicula');
+	console.log(entradas)
 
-console.log(moviesViewport);
+	entradas.forEach(entrada =>{
+		if (entrada.isIntersecting) {
+			pagina++;
+			loadMovies();
+		}
+	})
+}, optionsObserver);
 
 const loadMovies = async () => {
+	
 	try {
-		const response = await fetch(urlApi); //hacemos la peticion
+
+		const response = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=192e0b9821564f26f52949758ea3c473&language=es-MX&page=${pagina}`); //hacemos la peticion
+		
 		// Si la respuesta es correcta
 		if (response.status === 200) {
-			const data = await response.json();		// manejamos el JSON
-			let movies = '';
-			data.results.forEach((pelicula) => {	// iteramos sobre cada pelicula
+			
+			const data = await response.json(); // manejamos el JSON
+			
+			// iteramos sobre cada pelicula
+			data.results.forEach((pelicula) => {
+			
+			
 				// creamos un "componente" para mostrar cada una
-				movies += `
-					<div class="pelicula">
+			
+				peliculas += `<div class="pelicula">
 						<img class="poster" src="https://image.tmdb.org/t/p/w500/${pelicula.poster_path}">
 						<h3 class="titulo">${pelicula.title}</h3>
-					</div>
-				`;
+						</div>`;
 			});
-			// selesccionamos el contendor 
-			document.getElementById("contenedor").innerHTML = movies;
+
+			// selesccionamos el contendor
+			document.getElementById("contenedor").innerHTML = peliculas;
+			if (pagina < 1000) {			
+				if (ultimaPelicula) {
+					observador.unobserve(ultimaPelicula);
+				}
+	
+				// obtenemos el numero de peliculas en pantalla
+				
+				const moviesInViewport = document.querySelectorAll(
+					".contenedor .pelicula"
+				);
+				
+				ultimaPelicula = moviesInViewport[moviesInViewport.length - 1];
+				
+			}
+			observador.observe(ultimaPelicula);
 		} else if (response.status === 401) {
+			
 			alert("Pusiste la llave mal");
 		} else if (response.status === 404) {
+			
 			alert("La pelicula que buscas no existe");
 		} else {
+			
 			alert("Hubo un error y no sabemos que paso");
 		}
-	} catch (error) {		//atrapamos el error
+	} catch (error) {
+		
+		//atrapamos el error
 		console.log(error);
 	}
 };
 
 loadMovies();
+
