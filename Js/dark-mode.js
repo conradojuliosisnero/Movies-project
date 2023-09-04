de4651cf5a77846d1ff2b154343d8a15306cac08
conrado julio <conradojuliosisnero@gmail.com>
2023-09-04 12:24:18 -0500

    agregando estado del boton en local storage

--- Js/dark-mode.js
+++ Js/dark-mode.js
@@ -1,27 +1,51 @@
-// selects 
-const btndarkMode = document.querySelector('.bb8-toggle__container');
-const nav = document.querySelector('.nav');
+// selects
+const btndarkMode = document.querySelector(".input");
+const nav = document.querySelector(".nav");
+const slaider = document.querySelector(".slider");
 
+
+// listener de modo oscuro
 btndarkMode.addEventListener("click", () => {
-    document.body.classList.toggle("modo-oscuro");
-    nav.classList.toggle("nav-dark");
-    btndarkMode.classList.toggle("active");
-  
-    // modo guardado en localstorage
-    if (document.body.classList.contains("modo-oscuro")) {
-      localStorage.setItem("oscuro", "true");
-    } else {
-      localStorage.setItem("oscuro", "false");
-    }
-  });
-  
-  // comprovacion del localstore
-  if (localStorage.getItem("oscuro") === "true") {
-    document.body.classList.add("modo-oscuro");
-    nav.classList.add("nav-dark");
-    btndarkMode.classList.add("");
-  } else {
-    document.body.classList.remove("modo-oscuro");
-    nav.classList.remove("nav-dark");
-    btndarkMode.classList.remove("");
-  }
+	document.body.classList.toggle("modo-oscuro");
+	nav.classList.toggle("nav-dark");
+    
+	// modo guardado en localstorage modo oscuro
+	if (document.body.classList.contains("modo-oscuro")) {
+		localStorage.setItem("oscuro", "true");
+	} else {
+		localStorage.setItem("oscuro", "false");
+	}
+});
+
+// comprovacion del localstore del modo oscuro
+if (localStorage.getItem("oscuro") === "true") {
+	document.body.classList.add("modo-oscuro");
+	nav.classList.add("nav-dark");
+} else {
+	document.body.classList.remove("modo-oscuro");
+	nav.classList.remove("nav-dark");
+}
+
+// BOTON EN MODO SOCURO 
+
+// listener de modo oscuro
+btndarkMode.addEventListener("click", () => {
+    btndarkMode.classList.toggle("dark-btn");
+    slaider.classList.toggle("dark-btn");
+
+	// modo guardado en localstorage modo oscuro
+	if (btndarkMode.classList.contains("dark-btn")) {
+		localStorage.setItem("dark-btn", "true");
+	} else {
+		localStorage.setItem("dark-btn", "false");
+	}
+});
+
+// comprovacion del localstore del modo oscuro
+if (localStorage.getItem("dark-btn") === "true") {
+    btndarkMode.classList.add("dark-btn");
+    slaider.classList.add("dark-btn");
+} else {
+    btndarkMode.classList.remove("dark-btn");
+    slaider.classList.remove("dark-btn");
+}
